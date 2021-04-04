e93648f66281e3882d49526532cb89abcd17e5bf
harjot-saroya <harjot_saroya@homail.com>
2021-04-04 14:20:23 -0400

    added onclick scroll

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,14 +1,15 @@
-import react from "react"
+import {react} from "react"
 import testlogo from '../../assets/peerone.jpeg'
 import styles from './header.css'
 
 
 const Header = () => {
+     
     return(
         <div className="elements">
             <div className="browsecomponent">
                 <img src={testlogo} id="logo" />
-                <a id="browsebutton" href="google.ca">Browse Mentors</a>
+                <a id="browsebutton" href="" onClick={(e)=>{e.preventDefault();window.scrollTo({top:800,behavior:'smooth'})}}>Browse Mentors</a>
             </div>
         </div>
     )
