f20de0feca4f71c9ab1bde677c783506daf50155
harjot-saroya <harjot_saroya@homail.com>
2021-04-04 23:10:07 -0400

    small css fixes

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,6 +1,6 @@
 import {react} from "react"
 import testlogo from '../../assets/peerone.jpeg'
-import styles from './header.css'
+import'./header.css'
 
 
 const Header = () => {
@@ -9,7 +9,7 @@ const Header = () => {
         <div className="elements">
             <div className="browsecomponent">
                 <img src={testlogo} id="logo" />
-                <a id="browsebutton" href="" onClick={(e)=>{e.preventDefault();window.scrollTo({top:800,behavior:'smooth'})}}>Browse Mentors</a>
+                <a id="browsebutton" href="" onClick={(e)=>{e.preventDefault();window.scrollTo({top:750,behavior:'smooth'})}}>Browse Mentors</a>
             </div>
         </div>
     )
