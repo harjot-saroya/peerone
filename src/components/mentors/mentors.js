c65023b8302cc4e839fd89356c51324e0c91256c
SaiOduri <odurisai@gmail.com>
2021-06-03 01:01:34 -0400

    ♻️  Fixed Carousel Bugs

--- src/components/mentors/mentors.js
+++ src/components/mentors/mentors.js
@@ -17,7 +17,7 @@ const Mentors = () => {
                 <span>Featured mentors</span>
             </div>
             <div className="caro-container">
-                <Carousel centerSlidePercentage={33} centerMode showStatus={false} showArrows={true}>
+                <Carousel centerSlidePercentage={33} centerMode={true} showStatus={false} showIndicators={false} selectedItem={1}>
                         <div className="slide">
                             <div className="photo-cont">
                             <img id="photos"src={julia} />
